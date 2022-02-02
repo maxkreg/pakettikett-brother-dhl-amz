a738a1ac7eecfa29dbbf3784a02e66f9e15cc760
maxkreg <71139234+maxkreg@users.noreply.github.com>
2022-02-02 13:33:51 +0100

    added logging, added dhl_label routine

---
+++ index_dhl.js
@@ -0,0 +1,108 @@
+const puppeteer = require('puppeteer');
+
+const fs = require('fs');
+const https = require('https');
+
+const glob = require("glob");
+
+(async () => {
+
+//init
+    const browser = await puppeteer.launch({headless: true});
+    const page = await browser.newPage();
+
+//conversion sequence//
+//open website
+await page.goto('https://paketikett.de');
+console.log('opened paketikett website');
+
+
+//find pdf in downloads
+glob("/Users/Maxi/Downloads/DHL-Paketmarke_*.pdf", function (er, file) {
+    globalThis.globFile = file;
+    //debug 
+    //console.log(globFile.join(''));
+    console.log('found file:' + ' ' + globFile.join(''));
+
+  });
+
+
+//select label type
+await page.select('#dienstleister', 'DHL_privat4');
+console.log('selected label type');
+
+
+//upload asset
+const elementHandle = await page.$("input[name=dateiupload]");
+console.log('selected file upload selector');
+
+await elementHandle.uploadFile(globFile.join(''));
+console.log('uploading file');
+
+
+await page.click('button'); 
+console.log('clicked submit button');
+
+
+//download jpgs
+await page.waitForTimeout(2000);
+const imgUrl = await page.$eval('.w3-border, .w3-border-blue', img => img.src);
+        
+        https.get(imgUrl, res => {
+            const stream = fs.createWriteStream('output.jpg');
+            res.pipe(stream);
+            stream.on('finish', () => {
+                stream.close();
+            })
+        })
+console.log('downloaded output jpeg');
+
+//end conversion sequence//
+
+//print sequence//
+//init
+const page2 = await browser.newPage();
+
+//open website
+await page2.goto('http://100.122.136.54:8013/labeldesigner');
+console.log('opened brother web interface');
+
+
+//delete old asset
+await page2.click("button[id=deleteButton]");
+await page.waitForTimeout(4000);
+console.log('deleted old asset');
+
+//upload asset
+const elementHandle2 = await page2.$("input[name=upload]");
+console.log('selected file upload selector');
+
+await page.waitForTimeout(4000);
+await elementHandle2.uploadFile('output.jpg');
+console.log('uploaded file');
+
+await page.waitForTimeout(4000);
+await page2.click("input[name=submit]"); 
+console.log('clicked submit button');
+
+await page.waitForTimeout(4000);
+
+//print asset
+//await page2.click("button[id=printButton]"); 
+console.log('printed asset');
+await page.waitForTimeout(4000);
+
+//delete used asset
+await page2.click("button[id=deleteButton]"); 
+console.log('deleted asset');
+
+//end print sequence//
+
+
+
+//end
+await browser.close();
+})
+
+
+();
