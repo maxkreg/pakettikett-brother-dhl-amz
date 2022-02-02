6fd8ed5046469c54ad136a64dcf189b086abb87c
maxkreg <71139234+maxkreg@users.noreply.github.com>
2022-02-02 13:39:36 +0100

    added/fixed logs

--- index.js
+++ index.js
@@ -17,14 +17,21 @@ const https = require('https');
 //conversion sequence//
 //open website
 await page.goto('https://paketikett.de');
+console.log('opened paketikett website');
 
 //select label type
 await page.select('#dienstleister', 'DHL_amz_retour');
+console.log('selected label type');
 
 //upload asset
   const elementHandle = await page.$("input[name=dateiupload]");
+  console.log('selected file upload selector');
+
   await elementHandle.uploadFile('/Users/Maxi/Downloads/ShipperLabel.gif');
-  await page.click('button'); 
+  console.log('uploading file');
+await page.click('button'); 
+console.log('clicked submit button');
+
 
 //download jpgs
 await page.waitForTimeout(2000);
@@ -37,7 +44,8 @@ const imgUrl = await page.$eval('.w3-border, .w3-border-blue', img => img.src);
                 stream.close();
             })
         })
-//end conversion sequence//
+        console.log('downloaded output jpeg');
+        //end conversion sequence//
 
 //print sequence//
 //init
@@ -45,26 +53,37 @@ const page2 = await browser.newPage();
 
 //open website
 await page2.goto('http://100.122.136.54:8013/labeldesigner');
+console.log('opened brother web interface');
 
 
 //delete old asset
 await page2.click("button[id=deleteButton]");
 await page.waitForTimeout(4000);
+console.log('deleted old asset');
 
 //upload asset
 const elementHandle2 = await page2.$("input[name=upload]");
+console.log('selected file upload selector');
+
 await page.waitForTimeout(4000);
 await elementHandle2.uploadFile('output.jpg');
+console.log('uploaded file');
+
 await page.waitForTimeout(4000);
 await page2.click("input[name=submit]"); 
+console.log('clicked submit button');
+
 await page.waitForTimeout(4000);
 
 //print asset
 await page2.click("button[id=printButton]"); 
+console.log('printed asset');
 await page.waitForTimeout(4000);
 
 //delete used asset
 await page2.click("button[id=deleteButton]"); 
+console.log('deleted used asset');
+
 //end print sequence//
 
 
